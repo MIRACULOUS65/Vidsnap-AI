9c3ad191805ccae0f5768137500b7f59df4d9911
Sushovan Ghosh <sushovan1908@gmail.com>
2025-11-01 17:26:32 +0530

    Done making it now moving towards deployment

--- generate_process.py
+++ generate_process.py
@@ -1,20 +1,33 @@
 #this file looks for new folders inside user uploads and converts them to reel if they are not already converted
 import os
+from text_to_audio import text_to_speech_file
+import time
+import subprocess
+
 def text_to_audio(folder):
     print("text_to_audio",folder)
+    with open(f"user_uploads/{folder}/desc.txt") as f:
+        text =f.read()
+    print(text,folder)
+    text_to_speech_file(text,folder)
 
 def create_reel(folder):
+    command=f'''ffmpeg -f concat -safe 0 -i user_uploads/{folder}/input.txt -i user_uploads/{folder}/audio.mp3 -vf "scale=1080:1920:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2:black" -c:v libx264 -c:a aac -shortest -r 30 -pix_fmt yuv420p static/reels/{folder}.mp4'''
+    subprocess.run(command, shell=True,check=True)
     print("create_reel",folder)
 
 if __name__=="__main__":
-    with open("done.txt","r") as f:
-        done_folders=f.readlines()
+    while True:
+        print("Processing Queue......")
+        with open("done.txt","r") as f:
+            done_folders=f.readlines()
 
-    done_folders=[f.strip() for f in done_folders] #need coz if the folders are already processed they will not be needed to be processed again
-    folders= os.listdir("user_uploads")
-    for folder in folders:
-        if (folder not in done_folders):
-            text_to_audio(folder) #generate the audio from desc.txt
-            create_reel(folder) #convert the images and audio.mp3 inside the folder to a reel
-            with open("done.txt","a") as f:
-                f.write(folder+"\n")
+        done_folders=[f.strip() for f in done_folders] #need coz if the folders are already processed they will not be needed to be processed again
+        folders= os.listdir("user_uploads")
+        for folder in folders:
+            if (folder not in done_folders):
+                text_to_audio(folder) #generate the audio from desc.txt
+                create_reel(folder) #convert the images and audio.mp3 inside the folder to a reel
+                with open("done.txt","a") as f:
+                    f.write(folder+"\n")
+        time.sleep(4)
--- main.py
+++ main.py
@@ -20,30 +20,42 @@ def home():
 @app.route("/create",methods=["GET", "POST"])
 def create():
         myid=uuid.uuid1()
+        input_files=[]  # Initialize outside the if block
         if request.method == "POST":
             print(request.files.keys())
             rec_id=request.form.get("uuid")
             desc = request.form.get("text")
+            
             for key,value in request.files.items():
-             print(key,value)    
-            #Upload the file
-            file=request.files[key]
-            if file:
-                filename = secure_filename(file.filename)
-            if(not(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'],rec_id)))):
-                os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'],rec_id))
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'],rec_id, filename))
+                print(key,value)    
+                #Upload the file
+                file=request.files[key]
+                if file:
+                    filename = secure_filename(file.filename)
+                    if(not(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'],rec_id)))):
+                        os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'],rec_id))
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'],rec_id, filename))
+                    input_files.append(file.filename)
 
             #capture the description
             with open(os.path.join(app.config['UPLOAD_FOLDER'],rec_id, "desc.txt"),"w") as f:
                 f.write(desc)
 
+            # Move the file processing inside the POST block
+            for fl in input_files:
+                with open(os.path.join(app.config['UPLOAD_FOLDER'],rec_id, "input.txt"),"a") as f:
+                    f.write(f"file '{fl}'\nduration 1\n")
+                
+
         return render_template("create.html",myid=myid)
+        
 
 
 
 @app.route("/gallery")
 def gallery():
-    return render_template("gallery.html")
+    reels= os.listdir("static/reels")
+    print(reels)
+    return render_template("gallery.html",reels=reels)
 
 app.run(debug=True)
